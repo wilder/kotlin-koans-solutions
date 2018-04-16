70f4dcd31f2e447d6b0111d7744213618e019304
Wilder Pereira <wilder_roberto@hotmail.com>
2018-04-15 22:34:46 -0300

    Complete Task 7 - Nullable Types

--- src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
+++ src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
@@ -25,7 +25,8 @@ fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO
 fun sendMessageToClient(
         client: Client?, message: String?, mailer: Mailer
 ) {
-    todoTask7(client, message, mailer)
+    if (client?.personalInfo?.email == null || message == null) return
+    else mailer.sendMessage(client.personalInfo.email, message)
 }
 
 class Client (val personalInfo: PersonalInfo?)

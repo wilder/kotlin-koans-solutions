aac1db66064afdbaf5f90025ff443bcef44b5eed
Wilder Pereira <wilder_roberto@hotmail.com>
2018-05-26 13:43:27 -0300

    Complete task 10 - Object Expressions

--- src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -18,6 +18,17 @@ fun todoTask10(): Nothing = TODO(
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
+    Collections.sort(arrayList, object: Comparator<Int> {
+        override fun compare(o1: Int, o2: Int): Int = o2.compareTo(o1)
+    })
+    // Same as Collections.sort(arrayList, {o1, o2 -> o2.compareTo(o1))}
+    // or in Kotlin arrayList.reversed()
     return arrayList
+}
+
+
+class MyClass {
+    companion object Factory {
+        fun create(): MyClass = MyClass()
+    }
 }

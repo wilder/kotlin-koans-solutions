2b36f79bc3a6540cf2adbcefa478c273a522b495
Wilder Pereira <wilder_roberto@hotmail.com>
2018-05-26 16:53:01 -0300

    Complete task 17 - maxBy and minBy

--- src/ii_collections/n17MaxMin.kt
+++ src/ii_collections/n17MaxMin.kt
@@ -1,5 +1,6 @@
 package ii_collections
 
+//TODO: Add minBy
 fun example4() {
     val max = listOf(1, 42, 4).max()
     val longestString = listOf("a", "b").maxBy { it.length }
@@ -7,10 +8,10 @@ fun example4() {
 
 fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? {
     // Return a customer whose order count is the highest among all customers
-    todoCollectionTask()
+    return this.customers.maxBy { it.orders.count() }
 }
 
 fun Customer.getMostExpensiveOrderedProduct(): Product? {
     // Return the most expensive product which has been ordered
-    todoCollectionTask()
+    return this.orderedProducts.maxBy { it.price }
 }
